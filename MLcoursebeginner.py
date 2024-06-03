b820743441fb6a6dfa254674038ef134c665c2b2
tthiel1 <taranthielemann@gmail.com>
2024-06-02 20:19:30 -0400

    Update MLcoursebeginner.py

--- MLcoursebeginner.py
+++ MLcoursebeginner.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 data = pd.read_csv("housing.csv")
 
@@ -12,19 +13,51 @@ data.dropna(inplace=True)
 x = data.drop(['median_house_value'], axis=1)
 y = data['median_house_value']
 
-x_train, y_trian, y_train, y_test = train_test_split(x,y, test_size=0.2)
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.2)
 
 train_data = x_train.join(y_train)
 
-sns.heatmap(train_data.corr(numeric_only=True), annot=True, cmap="YlGnBu")
+
 
 train_data['total_rooms'] = np.log(train_data['total_rooms'] + 1)
 train_data['total_bedrooms'] = np.log(train_data['total_bedrooms'] + 1)
 train_data['population'] = np.log(train_data['population'] + 1)
 train_data['households'] = np.log(train_data['households'] + 1)
 
-train_data.hist()
+#train_data.hist()
 
-plt.show()
+train_data = train_data.join(pd.get_dummies(train_data.ocean_proximity)).drop(['ocean_proximity'], axis=1)
+
+
+plt.figure(figsize=(15,8))
+
+train_data['bedroom_ratio'] = train_data['total_bedrooms'] / train_data['total_rooms']
+train_data['household_rooms'] = train_data['total_rooms'] / train_data['households']
+
+#sns.scatterplot(x='latitude', y='longitude', data=train_data , hue='median_house_value', palette='coolwarm')
+
+sns.heatmap(train_data.corr(), annot=True, cmap="YlGnBu")
+
+
+x_train, y_train = train_data.drop(['median_house_value'], axis=1), train_data['median_house_value']
+
+reg = LinearRegression()
+
+reg.fit(x_train, y_train)
+
+test_data = x_test.join(y_test)
+
+test_data['bedroom_ratio'] = test_data['total_bedrooms'] / test_data['total_rooms']
+test_data['household_rooms'] = test_data['total_rooms'] / test_data['households']
+test_data['total_rooms'] = np.log(test_data['total_rooms'] + 1)
+test_data['total_bedrooms'] = np.log(test_data['total_bedrooms'] + 1)
+test_data['population'] = np.log(test_data['population'] + 1)
+test_data['households'] = np.log(test_data['households'] + 1)
+test_data = test_data.join(pd.get_dummies(test_data.ocean_proximity)).drop(['ocean_proximity'], axis=1)
+x_test, y_test = test_data.drop(['median_house_value'], axis=1), test_data['median_house_value']
+
+reg.fit(x_test, y_test)
+
+print(reg.score(x_test, y_test))
 
 
